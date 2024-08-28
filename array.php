13359eff2e01abe0aec8c135d104e3c8accaafa0
melia dilasari <meliadila1396@gmail.com>
2024-08-28 09:39:51 +0700

    commit 2 - finish array PHP questions

--- array.php
+++ array.php
@@ -19,8 +19,15 @@
             Kids : "Mike", "Dustin", "Will", "Lucas", "Max", "Eleven" 
             Adults: "Hopper", "Nancy",  "Joyce", "Jonathan", "Murray"
         */
-    $kids; // Lengkapi di sini
-    $adults;
+    $kids = ["Mike", "Dustin", "Will", "Lucas", "Max", "Eleven"]; // Lengkapi di sini
+    $adults = ["Hopper", "Nancy",  "Joyce", "Jonathan", "Murray"];
+
+    echo "<strong>Kids:</strong><br>";
+    print_r($kids) ;
+    echo "<br><br>";
+    echo "<strong>Adults:</strong><br>";
+    print_r($adults) ;
+
     echo "<h3> Soal 2</h3>";
     /* 
             SOAL NO 2
@@ -28,18 +35,27 @@
         */
     echo "Cast Stranger Things: ";
     echo "<br>";
-    echo "Total Kids: "; // Berapa panjang array kids
+    echo "Total Kids: " . count($kids); // Berapa panjang array kids
     echo "<br>";
     echo "<ol>";
     echo "<li> $kids[0] </li>";
+    echo "<li> $kids[1] </li>";
+    echo "<li> $kids[2] </li>";
+    echo "<li> $kids[3] </li>";
+    echo "<li> $kids[4] </li>";
+    echo "<li> $kids[5] </li>";
     // Lanjutkan
 
     echo "</ol>";
 
-    echo "Total Adults: "; // Berapa panjang array adults
+    echo "Total Adults: " . count($adults); // Berapa panjang array adults
     echo "<br>";
     echo "<ol>";
     echo "<li> $adults[0] </li>";
+    echo "<li> $adults[1] </li>";
+    echo "<li> $adults[2] </li>";
+    echo "<li> $adults[3] </li>";
+    echo "<li> $adults[4] </li>";
     // Lanjutkan
 
     echo "</ol>";
@@ -107,6 +123,33 @@
                 )
             
         */
+
+        $bioStrangerThings = 
+        [
+            ["Name" => "Will Byers",
+            "Age" => 12,
+            "Aliases" => "Will the Wise",
+            "Status"=> "Alive"],
+
+            ["Name" => "Mike Wheeler",
+            "Age" => 12,
+            "Aliases" => "Dungeon Master",
+            "Status"=> "Alive"],
+
+            ["Name" => "Jim Hopper",
+            "Age" => 43,
+            "Aliases" => "Chief Hopper",
+            "Status"=> "Deceased"],
+
+            ["Name" => "Eleven",
+            "Age" => 12,
+            "Aliases" => "El",
+            "Status"=> "Alive"]
+        ];
+        echo "<h3> Soal 3</h3>";
+        echo "<pre>";
+         print_r($bioStrangerThings);
+         echo "</pre>";
     ?>
 </body>
 
